a311dab3862ec5e3b31805b5754837c6672a0665
jatin-wipro <66778854+jatin-wipro@users.noreply.github.com>
2021-07-18 22:42:01 +0530

    Create lambda-iam.tf

---
+++ Basic_UseCase/lambda-iam.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_role.id
+  policy = "${file("iam/lamda-policy.json")}"
+
+}
+
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = "${file("iam/lamda-assume-policy.json")}"
+
+}
